714f178260dc309ca26ae4a0a29ad861acfead16
Alphalejo <keatola16.ktl@gmail.com>
2023-09-15 08:43:43 -0500

    Starting EDA

--- notebooks/toolbox.py
+++ notebooks/toolbox.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 def read_sheets(data, skip_rows, sheets_list):
     
-    sheets = dict
-
+    sheets = {}
     sheets = { sheet_name: pd.read_excel(data, sheet_name=sheet_name, skiprows=skip_rows) for sheet_name in list(sheets_list)}
+
 
     return sheets
