b37af970c291e2aa884c26925a6d3025796e0b0a
Alphalejo <keatola16.ktl@gmail.com>
2023-09-17 17:17:42 -0500

    Added column 'SHEET' univariable analysis finished

--- notebooks/toolbox.py
+++ notebooks/toolbox.py
@@ -3,7 +3,12 @@ import pandas as pd
 def read_sheets(data, skip_rows, sheets_list):
     
     sheets = {}
-    sheets = { sheet_name: pd.read_excel(data, sheet_name=sheet_name, skiprows=skip_rows) for sheet_name in list(sheets_list)}
+    #sheets = { sheet_name: pd.read_excel(data, sheet_name=sheet_name, skiprows=skip_rows) for sheet_name in list(sheets_list)}
+    for sheet_name in list(sheets_list):
+        
+        df = pd.read_excel(data, sheet_name=sheet_name, skiprows=skip_rows)
+        df["SHEET"] = sheet_name
 
+        sheets[sheet_name] = df
 
     return sheets
